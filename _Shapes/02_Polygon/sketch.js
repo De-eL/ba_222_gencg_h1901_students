e12d12b0930db15984b9ad969c96e8fa5d5a1d5c
De-eL <47041919+De-eL@users.noreply.github.com>
2019-11-28 13:19:50 +0100

    Polygon Exercise setup and thumb update

--- _Shapes/02_Polygon/sketch.js
+++ _Shapes/02_Polygon/sketch.js
@@ -13,6 +13,18 @@ function draw() {
 
 }
 
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+
 function keyPressed() {
   if (key == 's' || key == 'S') saveThumb(650, 350);
 }
