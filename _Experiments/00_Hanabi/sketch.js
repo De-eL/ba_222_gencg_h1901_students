2a3735f4ec76c5d6014863b88eb5b11a34b2cb4d
De-eL <47041919+De-eL@users.noreply.github.com>
2019-12-03 17:55:50 +0100

    Update sketch.js

--- _Experiments/00_Hanabi/sketch.js
+++ _Experiments/00_Hanabi/sketch.js
@@ -1,35 +1,46 @@
 // Global var
   let rocket1;
-
+  let particleArray = [];
+  let hasRun = false;
 
  class rocket {
   constructor() {
+    this.speed = 18;
     this.x = width/2;
-    this.y = height;
+    this.y = height+this.speed;
     this.diameter = random(5, 15);
-    this.speed = 5;
   }
   move() {
     this.y -= this.speed;
   }
   display() {
-    ellipse(this.x, this.y, this.diameter, 2*this.diameter);
+    fill(204, 101, 192, 127);
+    stroke(127, 63, 120);
+    ellipse(this.x, this.y, this.diameter, 2 * this.diameter);
   }
   explode() {
     for (let i = 0; i < random(0, 10) + 10; i++) {
-
+      particleArray[i] = new particle(this.x, this.y);
+      console.log(particleArray[i]);
     }
   }
  }
 
- class particle() {
+ class particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.d = random(4, 6);
+    this.vx = random(-2, 2);
+    this.duration = random(1, 3);
   }
   move() {
-
+    this.x += this.vx;
+  }
+  display() {
+    fill(204, 101, 192, 127);
+    stroke(127, 63, 120);
+    ellipse(this.x, this.y, this.d, this.d);
   }
  }
 
@@ -45,11 +56,22 @@ function setup() {
 
 function draw() {
   background(0);
-  fill(204, 101, 192, 127);
-  stroke(127, 63, 120);
+
   rocket1.move();
   rocket1.display();
-  rocket1.speed *= 0.99;
+  rocket1.speed *= 0.97;
+  if (rocket1.speed < 0.15 && hasRun == false) {
+    rocket1.explode();
+    rocket1.diameter = 0;
+    hasRun = true;
+  }
+  if (particleArray.length > 0) {
+  for (var i = 0; i <= particleArray.length - 1; i++) {
+    particleArray[i].move();
+    particleArray[i].display();
+    console.log("updating particles");
+  }
+}
 }
 
 function keyPressed() {
